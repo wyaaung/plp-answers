92a0c02a281d2eb7a1dac1494b8f1487e87077bb
Wai Aung <2392waau@nibe.se>
2022-07-21 12:58:37 +0100

    Week 02 Ex3

---
+++ week02/03-variables/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int int_var = 42;
+    double double_var = 24;
+    printf("int_var: %d\n", int_var);
+    printf("double_var: %f\n", double_var);
+    return 0;
+}
