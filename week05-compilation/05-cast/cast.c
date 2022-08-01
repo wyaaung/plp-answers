e3d1632c3bef8d081939abf536cc7f7c97cfc575
wyaaung <wya.aung@gmail.com>
2022-08-01 21:02:44 +0100

    Minor Changes

--- week05-compilation/05-cast/cast.c
+++ week05-compilation/05-cast/cast.c
@@ -43,3 +43,6 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+// Run the below command to check answers; The one on github is outdated
+// Make sure you're in the same directory as this file when running the command
+// check50 -l --ansi-log olivierpierre/comp26020-problems/2021-2022/week5-compilation/05-cast
