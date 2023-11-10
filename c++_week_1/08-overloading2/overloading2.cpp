63ea5d38ea0d33f925181301fe0d711c5d65b654
wyaaung <wya.aung@gmail.com>
2023-11-10 23:34:26 +0000

    Added comment for Solution Checking

--- c++_week_1/08-overloading2/overloading2.cpp
+++ c++_week_1/08-overloading2/overloading2.cpp
@@ -33,3 +33,7 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+// Run the below command to check answers; The one on github is outdated
+// Make sure you're in the same directory as this file when running the command
+// check50 -l --ansi-log ppetoumenos/comp26020-problems/2023-2024/CPP_W1-Classes/08-overloading2
